41b6b6058f0e65bb10e69db737356ecf98c1dc5a
apardor <apardor@gmail.com>
2023-02-02 01:01:30 +0100

    testing AWS amplfify

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,7 +1,6 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import React from 'react'
 import Link from 'next/link'
 import { GetStaticProps } from 'next';
 import { IMovies } from '@/types/types';
