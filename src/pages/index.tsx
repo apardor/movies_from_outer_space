1f49ead84e8e55cbd72445d5977f2a40fba6d821
apardor <apardor@gmail.com>
2023-02-08 12:09:20 +0100

    testing new css layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ import { log } from 'console';
 
 
 
-
 const api_key = process.env.TMDB_API_KEY;
 const baseUrl = process.env.API_URL
 const imageDefaultEndPoint = 'https://image.tmdb.org/t/p/w500'
@@ -21,6 +20,7 @@ const [results, setResults] = React.useState(false);
 const [page, setPage] = React.useState(''); 
 const [search, setSearch] = React.useState(''); 
 const [query, setQuery] = React.useState('') ;
+const [scroll, setScroll] = React.useState(false) ;
 
 const searchMovie = async () =>{
   const request = await fetch (`https://api.themoviedb.org/3/search/movie?api_key=b7e763dc89359ad28e83964b5a12b539&query=${query}`)
@@ -28,25 +28,6 @@ const searchMovie = async () =>{
   setMovies(res.results);
   setPage(res.page)
 }
-
-
-
-const handleChange = (e:any) =>{
- setSearch(e.target.value);
-
-}
-
-const submitSearchMovie = (e: React.SyntheticEvent) =>{
-  e.preventDefault();
-  setResults(true)
-  setQuery(search)
-}
-
-
-useEffect(() => {
-  searchMovie()
-}, [query])
-
 
 const sciFiResults = [];
 
@@ -58,6 +39,35 @@ const sciFiFilter =  movies?.map((movie:any) => {
   }
 }) 
 
+const handleChange = (e:any) =>{
+ setSearch(e.target.value);
+}
+
+const scrollIntoResults = (id: string) => {
+  let element = document.getElementById(id) as HTMLElement;
+  if (!element) return;
+  element.scrollIntoView({
+      behavior: "smooth"
+  });
+};
+
+const submitSearchMovie = (e: React.SyntheticEvent) =>{
+  e.preventDefault();
+  setResults(true)
+  setQuery(search)
+  if( sciFiResults.length > 0) {
+  scrollIntoResults(styles.results__div)
+  }
+}
+
+
+
+useEffect(() => {
+  searchMovie()
+}, [query])
+
+
+
 
 
   return (
@@ -68,12 +78,10 @@ const sciFiFilter =  movies?.map((movie:any) => {
           <input className={styles.search__input}  name='query' type='search'  value={search} onChange={handleChange} />
           <button className={styles.search__button} >Search</button>
         </form>
-        { results ?
-         (sciFiResults.length === 0 ?  <h2 className={styles.search__no__results}> No results </h2> :  <h2 className={styles.search__results__length}>Results: {sciFiResults.length} 
-        </h2> ) : ''}
+        {  results ? (sciFiResults.length === 0 ?  <h2 className={styles.search__no__results}> No results </h2> : '') : ''}
         <div>
+        { sciFiResults.length > 0 ? <h2 id={styles.results__div} className={`${styles.search__results__length} ${styles.heading__h2}`}>Results: {sciFiResults.length}</h2> : ''}
       <div className={styles.search__results} >
-    
       {  sciFiResults ? (sciFiResults.map((movie:any) => {
               return (     
                 <>
@@ -83,9 +91,9 @@ const sciFiFilter =  movies?.map((movie:any) => {
                       <Image src={`${imageDefaultEndPoint}${movie.poster_path}`} alt={movie.original_title} 
                               width="0"
                               height="0"
-                              sizes="100vw"
-                              style={{ width: 'auto', height: 'auto' }}/>
-                        <h3><strong>{movie.original_title}</strong></h3> 
+                              sizes="100%"
+                              style={{ width: '100%', height: 'auto', borderRadius:'20px 20px 0 0' }}/>
+                        <h3 className={styles.heading__h3}><strong>{movie.original_title}</strong></h3> 
                         <p><strong>Rating: {movie.vote_average}</strong></p> 
                         <p><strong>Release date: {movie.release_date}</strong></p> 
                   </div>
