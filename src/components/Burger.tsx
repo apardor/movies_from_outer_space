2d40487947194b3db0da789f27df32c98b9fe6cf
apardor <apardor@gmail.com>
2023-02-15 12:32:48 +0100

    fixed import nav module and css

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styles from '@/styles/Burger.module.css'
+import Nav from '@/components/Nav';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faSpaghettiMonsterFlying} from "@fortawesome/free-solid-svg-icons";
 import Link from 'next/link';
@@ -28,15 +29,18 @@ const updateMenu = () =>{
   }
 
   return (
-    <nav className={styles.nav}>
+    <nav className={styles.main__nav}>
+      <div className={styles.nav__logo}>
       <Link href="/">
       <FontAwesomeIcon icon={faSpaghettiMonsterFlying} className={styles.nav__icon}/>
       </Link>
+      </div>
        <div className={styles.burger_menu} onClick={updateMenu}>
           <div className={styles.burgerClass}></div>
           <div className={styles.burgerClass}></div>
           <div className={styles.burgerClass}></div>
       </div>
+      <Nav open={open} closing={updateMenu}/>
     </nav>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import type { AppProps } from 'next/app'
 import '../styles/styles.css';
 import Burger from '@/components/Burger';
 import Footer from '@/components/Footer';
+import styles from '@/styles/App.module.css'
+
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<>
+  return (
+      <div className={styles.app__container}>
       <Burger />
       <Component {...pageProps} />
       <Footer />
-      </>
+      </div>
   )
 }
