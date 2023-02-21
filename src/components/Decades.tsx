4071db73df7d20db0c28972e417b52bdffaa39cb
apardor <apardor@gmail.com>
2023-02-21 11:48:56 +0100

    fixed mobile first

--- src/components/Decades.tsx
+++ src/components/Decades.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'next/link';
-import { IMovies } from '@/types/types';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css'
 import PaginationDecade from '@/components/PaginationDecade';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import '../styles/styles.css';
 import Burger from '@/components/Burger';
 import Footer from '@/components/Footer';
 import styles from '@/styles/App.module.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 
 
