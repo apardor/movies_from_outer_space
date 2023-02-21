6630df6ccd36f6e5aeb1103a4d2e20b3c1e2de1c
apardor <apardor@gmail.com>
2023-02-21 13:26:15 +0100

    fixed random movie styles on mobile

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -22,15 +22,14 @@ if(loading){
               <>
               <div key={movie.id} className={styles.card}>
               <Link key={movie.id} href="/[id]" as={`/${movie.id}`}>
-               
                     <Image src={`${imageDefaultEndPoint}/${movie.poster_path}`} alt={movie.original_title} 
                             width="0"
                             height="0"
                             sizes="100%"
                             style={{ width: '100%', height: 'auto', borderRadius:'20px 20px 0 0' }}/>
                       <h3 className={styles.heading__h3}><strong>{movie.original_title}</strong></h3> 
-                      <p><strong>Rating: {movie.vote_average}</strong></p> 
-                      <p><strong>Release date: {movie.release_date}</strong></p> 
+                      <p className={styles.p__movies}><strong>Rating: {movie.vote_average}</strong></p> 
+                      <p className={styles.p__movies}><strong>Release date: {movie.release_date}</strong></p> 
                 
               </Link>
             </div>  
@@ -40,15 +39,14 @@ if(loading){
    { random  ? <div className={styles.search__results__random}>
               <div key={random.id} className={styles.card__random}>
               <Link key={random.id} href="/[id]" as={`/${random.id}`}>
-                
                     <Image src={`${imageDefaultEndPoint}/${random.poster_path}`} alt={random.original_title} 
                             width="0"
                             height="0"
                             sizes="100%"
                             style={{ width: '100%', height: 'auto', borderRadius:'20px 20px 0 0' }}/>
                       <h3 className={styles.heading__h3}><strong>{random.original_title}</strong></h3> 
-                      <p><strong>Rating: {random.vote_average}</strong></p> 
-                      <p><strong>Release date: {random.release_date}</strong></p> 
+                      <p className={styles.p__movies}><strong>Rating: {random.vote_average}</strong></p> 
+                      <p className={styles.p__movies}><strong>Release date: {random.release_date}</strong></p> 
                
               </Link>
             </div> 
